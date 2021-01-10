f4144a70711c6f2b56bb97681dbfaae0e3f3c9e0
madelinemc <madeline.coons@gmail.com>
2021-01-10 12:08:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+// remove the <main> with id 'main'
+main.remove()
+
+// create an <h1> with id 'victory'").eql('H1')
+var newHeader = document.createElement('h1');
+
+// create an <h1> with id 'victory'").eql('victory')
+newHeader.setAttribute("id","victory");
+
+//create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+newHeader.innerHTML = "Madeline is the champion";
+
